df4bdec25e1ba4264ecd6307485137b508db1252
Endless Coding <endlesscoding@yahoo.com>
2026-02-20 21:11:25 +0300

    cr a/stls

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
 import { Route, Routes, Link } from "react-router-dom"
 import { HomePage } from "../pages/HomePage"
 import { ArticalsPage } from "../pages/ArticalsPage"
@@ -7,23 +7,27 @@ import { Counter } from "../widgets/Counter/Counter"
 interface AppProps {
   className?: string
 }
+type Theme = "light" | "dark"
+
 export const App = (
   { className }: AppProps
 ) => {
+  const [theme, setTheme] = useState<Theme>("dark")
   return (
-    <div className={`
+    <div className={`app app_${theme}_theme
       ${className ? className : undefined}
     `}>
-      <Link to={"/"}>{"Home"}</Link>
-      <Link to={"/articals"}>{"Articals"}</Link>
-      <Counter />
-      <Suspense fallback={<div>{"Loading..."}</div>}>
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>CHangeTHeme</button>
+        <Link to={"/"}>{"Home"}</Link>
+        <Link to={"/articals"}>{"Articals"}</Link>
+        <Counter />
+        <Suspense fallback={<div>{"Loading..."}</div>}>
 
-        <Routes>
-          <Route path={"/"} element={<HomePage />} />
-          <Route path={"/articals"} element={<ArticalsPage />} />
-        </Routes>
-      </Suspense>
+          <Routes>
+            <Route path={"/"} element={<HomePage />} />
+            <Route path={"/articals"} element={<ArticalsPage />} />
+          </Routes>
+        </Suspense>
     </div>
   )
 }
